f2f36948fe90983b34d8e15249b4f22ef8f992e2
twinther <tommy@winther.nu>
2010-10-24 22:07:41 +0200

    made __init__ work for scripts

--- script.module.danishaddons/danishaddons/__init__.py
+++ script.module.danishaddons/danishaddons/__init__.py
@@ -24,13 +24,14 @@ def parseParams(input):
 	return params
 
 # Initialize convenience constants
-ADDON_PATH = sys.argv[0]
-ADDON_ID = ADDON_PATH[9:-1]
-ADDON_HANDLE = int(sys.argv[1])
-ADDON = xbmcaddon.Addon(id = ADDON_ID)
-ADDON_DATA_PATH = xbmc.translatePath(ADDON.getAddonInfo("Profile"))
-ADDON_PARAMS = parseParams(sys.argv[2][1:])
+if(len(sys.argv) > 1):
+	ADDON_PATH = sys.argv[0]
+	ADDON_ID = ADDON_PATH[9:-1]
+	ADDON_HANDLE = int(sys.argv[1])
+	ADDON = xbmcaddon.Addon(id = ADDON_ID)
+	ADDON_DATA_PATH = xbmc.translatePath(ADDON.getAddonInfo("Profile"))
+	ADDON_PARAMS = parseParams(sys.argv[2][1:])
 
-# Create addon data path
-if(not os.path.isdir(os.path.dirname(ADDON_DATA_PATH))):
-	os.makedirs(os.path.dirname(ADDON_DATA_PATH))
+	# Create addon data path
+	if(not os.path.isdir(os.path.dirname(ADDON_DATA_PATH))):
+		os.makedirs(os.path.dirname(ADDON_DATA_PATH))
