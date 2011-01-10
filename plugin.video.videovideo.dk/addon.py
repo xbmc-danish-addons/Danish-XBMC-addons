bdc836f98d39fb04d920740b89f7e3cc4ab30976
twinther <tommy@winther.nu>
2011-01-10 17:48:29 +0100

    Rewrote to use json feeds from videovideo.dk

--- plugin.video.videovideo.dk/addon.py
+++ plugin.video.videovideo.dk/addon.py
@@ -7,82 +7,66 @@ import xbmcaddon
 import xbmcgui
 import xbmcplugin
 
-import feedparser
+import simplejson
 import danishaddons
 import danishaddons.web
+import feedparser
 
-SHOWS_URL = 'http://videovideo.dk/shows'
+SHOWS_URL = 'http://videovideo.dk/shows/json/'
+EPISODES_URL = 'http://videovideo.dk/episodes/json/show_id/%s'
 RSS_URL_TEMPLATE = 'http://videovideo.dk/show/%s/rss/720/mp4'
 
 def showOverview():
-    html_path = os.path.join(danishaddons.ADDON_DATA_PATH, 'shows.html')
-    html = danishaddons.web.downloadAndCacheUrl(SHOWS_URL, html_path, 24 * 60)
+    json_path = os.path.join(danishaddons.ADDON_DATA_PATH, 'shows.json')
+    json = danishaddons.web.downloadAndCacheUrl(SHOWS_URL, json_path, 24 * 60)
 
-    for m in re.finditer('<img src="(http://gfx.videovideo.dk/[^"]+)".*?<h5>([^<]+)</h5>.*?<p>([^<]+)</p>.*?href="/show/([^"]+)"', html, re.DOTALL):
-        icon = m.group(1)
-        title = m.group(2)
-        description = m.group(3)
-        slug = m.group(4)
-
-        item = xbmcgui.ListItem(title, iconImage = icon)
+    shows = simplejson.loads(json)
+    for show in shows:
+        item = xbmcgui.ListItem(show['title'], iconImage = show['image'])
         item.setInfo(type = 'video', infoLabels = {
-            'title' : title,
-            'plot' : description
+            'title' : show['title'],
+            'plot' : show['description']
         })
-        url = danishaddons.ADDON_PATH + '?slug=' + slug
+        item.setProperty('Fanart_Image', show['imagefull'])
+
+        m = re.search('([0-9]+)$', show['url'])
+        show_id = m.group(1)
+        
+        url = danishaddons.ADDON_PATH + '?id=' + show_id
         xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item, True)
 
     xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
+    
+def showShow(id):
+    json_path = os.path.join(danishaddons.ADDON_DATA_PATH, 'episodes_%s.json' % id)
+    json = danishaddons.web.downloadAndCacheUrl(EPISODES_URL % id, json_path, 24 * 60)
 
-def showShow(slug):
-    rss = danishaddons.web.downloadAndCacheUrl(RSS_URL_TEMPLATE % slug, os.path.join(danishaddons.ADDON_DATA_PATH, '%s.rss' % slug), 60)
-    d = feedparser.parse(rss)
+    episodes = simplejson.loads(json)
+    for episode in episodes:
+        item = xbmcgui.ListItem(episode['title'], iconImage = episode['image'])
 
-    for e in d.entries:
-        iconPath = os.path.join(danishaddons.ADDON_DATA_PATH, '%s.jpg' % e.id)
-        if not os.path.exists(iconPath):
-            cacheIcon(e.link, iconPath)
-
-        item = xbmcgui.ListItem(e.title, iconImage = iconPath)
-        try:
-            description = danishaddons.web.decodeHtmlEntities(e.description)
-            description = re.sub('<.*?>', '', description)
-            description = description.replace('\t', '')
-        except AttributeError:
-            description = 'Ingen beskrivelse'
+        date = '%s.%s.%s' % (episode['timestamp'][8:10], episode['timestamp'][5:7], episode['timestamp'][0:4])
 
         infoLabels = {
-            'tvshowtitle' : d.channel.title,
-            'title' : e.title,
-            'plot' : description,
+            'title' : episode['title'],
+            'plot' : episode['shownotes'],
+            'date' : date,
+            'aired' : episode['timestamp'][0:11],
+            'year' : int(episode['timestamp'][0:4])
         }
-        if e.updated_parsed is not None:
-            infoLabels['aired'] = parsePubDate(e.updated_parsed)
-            infoLabels['date'] = parseDate(e.updated_parsed)
-            infoLabels['year'] = e.updated_parsed[0]
-
         item.setInfo('video', infoLabels)
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, e.enclosures[0].href, item, False)
+        item.setProperty('Fanart_Image', episode['imagefull'])
+        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, episode['distributions']['720'], item, False)
 
     xbmcplugin.addSortMethod(danishaddons.ADDON_HANDLE, xbmcplugin.SORT_METHOD_DATE)
     xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
 
-def parsePubDate(pubDate):
-    return '%d-%d-%d' % (pubDate[0], pubDate[1], pubDate[2])
-
-def parseDate(pubDate):
-    return '%02d.%02d.%d' % (pubDate[2], pubDate[1], pubDate[0])
-
-def cacheIcon(url, cachePath):
-    html = danishaddons.web.downloadUrl(url)
-    m = re.search('poster="(.*?)"', html)
-    danishaddons.web.downloadAndCacheUrl(m.group(1), cachePath, 60)
 
 if __name__ == '__main__':
     danishaddons.init(sys.argv)
 
-    if danishaddons.ADDON_PARAMS.has_key('slug'):
-        showShow(danishaddons.ADDON_PARAMS['slug'])
+    if danishaddons.ADDON_PARAMS.has_key('id'):
+        showShow(danishaddons.ADDON_PARAMS['id'])
     else:
         showOverview()
 
