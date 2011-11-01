2eff1bb9420d699429145e971fef1f065df35438
twinther <tommy@winther.nu>
2011-11-01 19:59:00 +0100

    removed use of script.module.danishaddons

--- plugin.video.vegaplayer.dk/addon.py
+++ plugin.video.vegaplayer.dk/addon.py
@@ -1,12 +1,11 @@
-import os
 import sys
+import urllib2
+import cgi as urlparse
 
 import xbmc
 import xbmcgui
 import xbmcplugin
-
-import danishaddons
-import danishaddons.web
+import xbmcaddon
 
 from elementtree import ElementTree
 
@@ -15,96 +14,104 @@ CONCERTS_URL = BASE_URL % 'concerts.jsp'
 CONCERT_URL = BASE_URL % 'concert.jsp?id=%s'
 RTMP_URL = 'rtmp://vegasrv1.dedicated.cohaesio.net/vod/vod/%s'
 
-def listConcerts():
-    concertsXml = danishaddons.web.downloadAndCacheUrl(CONCERTS_URL,
-            os.path.join(danishaddons.ADDON_DATA_PATH, 'concerts.xml'), 24 * 60)
-    doc = ElementTree.fromstring(concertsXml)
+class VegaPlayerAddon(object):
+    def listConcerts(self):
+        concertsXml = self.downloadUrl(CONCERTS_URL)
+        doc = ElementTree.fromstring(concertsXml)
 
-    for concert in doc.findall('concert'):
-        infoLabels = {
-            'plotoutline': concert.findtext('venue'),
-            'date' : concert.findtext('date'),
-            'year' : int(concert.findtext('date')[-4:]),
-            'title' : concert.findtext('name') + ' - ' + concert.findtext('date')
-        }
+        for concert in doc.findall('concert'):
+            infoLabels = {
+                'plotoutline': concert.findtext('venue'),
+                'date' : concert.findtext('date'),
+                'year' : int(concert.findtext('date')[-4:]),
+                'title' : concert.findtext('name') + ' - ' + concert.findtext('date')
+            }
 
-        iconImage = BASE_URL % concert.findtext('teaserimage')
-        item = xbmcgui.ListItem(infoLabels['title'], iconImage=iconImage)
-        item.setProperty('Fanart_Image', iconImage)
-        item.setInfo('video', infoLabels)
-        url = danishaddons.ADDON_PATH + '?uuid=' + concert.attrib.get('id')
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item, isFolder=True)
+            teaserImage = BASE_URL % concert.findtext('teaserimage')
+            item = xbmcgui.ListItem(infoLabels['title'], iconImage=teaserImage, thumbnailImage=teaserImage)
+            item.setProperty('Fanart_Image', teaserImage)
+            item.setInfo('video', infoLabels)
+            url = PATH + '?uuid=' + concert.attrib.get('id')
+            xbmcplugin.addDirectoryItem(HANDLE, url, item, isFolder=True)
 
-    xbmcplugin.addSortMethod(danishaddons.ADDON_HANDLE, xbmcplugin.SORT_METHOD_TITLE)
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
-
-
-def listConcert(uuid):
-    concertXml = danishaddons.web.downloadAndCacheUrl(CONCERT_URL % uuid,
-            os.path.join(danishaddons.ADDON_DATA_PATH, 'concert_%s.xml' % uuid), 24 * 60)
-    doc = ElementTree.fromstring(concertXml)
-
-    iconImage = BASE_URL % doc.findtext('concert/teaserimage')
-    description = doc.findtext('concert/name') + ' - ' + (doc.findtext('concert/venue') + ' - ' + doc.findtext('concert/date'))
-    artistPrefix = doc.findtext('concert/name') + ' - %s'
-    startTimes = list()
-    for track in doc.findall('tracks/track'):
-        startTimes.append(int(track.attrib.get('startms')) / 1000)
-    startTimes.append(startTimes[len(startTimes)-1])
-
-    idx = 0
-    for track in doc.findall('tracks/track'):
-        startSeconds = int(track.attrib.get('startms')) / 1000
-        durationSeconds = startTimes[idx + 1] - startSeconds
-
-        infoLabels = {
-            'title' : artistPrefix % track.findtext('name'),
-            'duration' : str(durationSeconds / 60),
-            'plotoutline' : description
-        }
-
-        item = xbmcgui.ListItem(infoLabels['title'], iconImage=iconImage)
-        item.setProperty('Fanart_Image', iconImage)
-        item.setInfo('video', infoLabels)
-        url = danishaddons.ADDON_PATH + '?uuid=%s&start=%d' % (uuid, startSeconds)
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item)
-
-        idx += 1
-
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
+        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_TITLE)
+        xbmcplugin.endOfDirectory(HANDLE)
 
 
-def playVideo(uuid, startSeconds):
-    concertXml = danishaddons.web.downloadAndCacheUrl(CONCERT_URL % uuid,
-            os.path.join(danishaddons.ADDON_DATA_PATH, 'concert_%s.xml' % uuid), 24 * 60)
-    doc = ElementTree.fromstring(concertXml)
+    def listConcert(self, uuid):
+        concertXml = self.downloadUrl(CONCERT_URL % uuid)
+        doc = ElementTree.fromstring(concertXml)
 
-    description = doc.findtext('concert/name') + ' - ' + (doc.findtext('concert/venue') + ' - ' + doc.findtext('concert/date'))
-    url = RTMP_URL % doc.findtext('concert/stream')[4:].replace(' ', '%20')
+        teaserImage = BASE_URL % doc.findtext('concert/teaserimage')
+        description = doc.findtext('concert/name') + ' - ' + (doc.findtext('concert/venue') + ' - ' + doc.findtext('concert/date'))
+        artistPrefix = doc.findtext('concert/name') + ' - %s'
+        startTimes = list()
+        for track in doc.findall('tracks/track'):
+            startTimes.append(int(track.attrib.get('startms')) / 1000)
+        startTimes.append(startTimes[len(startTimes)-1])
 
-    iconImage = BASE_URL % doc.findtext('concert/teaserimage')
-    item = xbmcgui.ListItem(description, thumbnailImage = iconImage)
+        for idx, track in enumerate(doc.findall('tracks/track')):
+            startSeconds = int(track.attrib.get('startms')) / 1000
+            durationSeconds = startTimes[idx + 1] - startSeconds
 
-    player = xbmc.Player()
-    player.play(url, item)
+            infoLabels = {
+                'title' : artistPrefix % track.findtext('name'),
+                'duration' : str(durationSeconds),
+                'plotoutline' : description
+            }
 
-    startTime = int(startSeconds)
-    if startTime > 0:
-        for i in range(5):
-            if player.isPlaying():
-                player.seekTime(startTime)
-                break
-            xbmc.sleep(1000)
+            item = xbmcgui.ListItem(infoLabels['title'], iconImage=teaserImage, thumbnailImage=teaserImage)
+            item.setProperty('Fanart_Image', teaserImage)
+            item.setInfo('video', infoLabels)
+            url = PATH + '?uuid=%s&start=%d' % (uuid, startSeconds)
+            #url = RTMP_URL % doc.findtext('concert/stream')[4:].replace(' ', '%20')
+            #if startSeconds > 0:
+            #    url += " start=%d" % startSeconds
+            xbmcplugin.addDirectoryItem(HANDLE, url, item)
+
+        xbmcplugin.endOfDirectory(HANDLE)
+
+
+    def playVideo(self, uuid, startSeconds):
+        concertXml = self.downloadUrl(CONCERT_URL % uuid)
+        doc = ElementTree.fromstring(concertXml)
+
+        description = doc.findtext('concert/name') + ' - ' + (doc.findtext('concert/venue') + ' - ' + doc.findtext('concert/date'))
+        url = RTMP_URL % doc.findtext('concert/stream')[4:].replace(' ', '%20')
+
+        teaserImage = BASE_URL % doc.findtext('concert/teaserimage')
+        item = xbmcgui.ListItem(description, iconImage=teaserImage, thumbnailImage=teaserImage)
+
+        player = xbmc.Player()
+        player.play(url, item)
+
+        startTime = int(startSeconds)
+        if startTime > 0:
+            for i in range(5):
+                if player.isPlaying():
+                    player.seekTime(startTime)
+                    break
+                xbmc.sleep(1000)
+
+    def downloadUrl(self, url):
+        u = urllib2.urlopen(url)
+        contents = u.read()
+        u.close()
+        return contents.replace('encoding="iso-8859-1"', 'encoding="utf-8"')
 
 if __name__ == '__main__':
-    danishaddons.init(sys.argv)
+    ADDON = xbmcaddon.Addon(id = 'plugin.video.vegaplayer.dk')
+    PATH = sys.argv[0]
+    HANDLE = int(sys.argv[1])
+    PARAMS = urlparse.parse_qs(sys.argv[2][1:])
 
-    if danishaddons.ADDON_PARAMS.has_key('start'):
-        playVideo(danishaddons.ADDON_PARAMS['uuid'], danishaddons.ADDON_PARAMS['start'])
+    vp = VegaPlayerAddon()
+    if PARAMS.has_key('start'):
+        vp.playVideo(PARAMS['uuid'][0], PARAMS['start'][0])
 
-    elif danishaddons.ADDON_PARAMS.has_key('uuid'):
-        listConcert(danishaddons.ADDON_PARAMS['uuid'])
+    elif PARAMS.has_key('uuid'):
+        vp.listConcert(PARAMS['uuid'][0])
 
     else:
-        listConcerts()
+        vp.listConcerts()
 
